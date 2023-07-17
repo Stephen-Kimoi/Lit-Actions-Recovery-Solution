650b456a2b97b16c8f1a52a888bc210f62d30f83
Stephen Kimoi <moenisteve@gmail.com>
2023-07-17 13:22:42 +0300

    function for generating OTP

--- AT_sms_api/sendSMS.js
+++ AT_sms_api/sendSMS.js
@@ -14,5 +14,19 @@ const sms = AfricasTalking.SMS;
 
 module.exports = function generateOTP(phoneNumber) {
     const otp = Math.floor(1000 + Math.random() * 900000).toString(); 
-    const message = `Your verification code is ${otp}. Enter this code in PKP to complete your login.`
+    const message = `Your verification code is ${otp}. Enter this code in PKP to complete your login.`; 
+
+    const options = {
+        to: [phoneNumber], 
+        message: message
+    }
+
+    return sms.send(options)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.error(error)
+            return null; 
+        })
 }
